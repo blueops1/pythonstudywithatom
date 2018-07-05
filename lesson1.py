21f97425f9f8cab423d3a70fbade1b465f6402c9
blueops1 <hyzfgjj@163.com>
2018-07-05 16:08:22 +0800

    lesson3-4

--- lesson1.py
+++ lesson1.py
@@ -1,2 +1,16 @@
 print 'lesson1!'
 print 'new branch of atom'
+import cv2 as cv
+img = cv.imread("logo.jpg",0)
+#result = cv.imread("test.jpg")
+#print img
+#cv.Canny(img,result,50,150)
+#cv.imwrite("test1.jpg",result)
+#cv.namedWindow("aaa",0)
+cv.imshow("aaa",img)
+k = cv.waitKey(0)
+if k == 27:
+    cv.destroyAllWindows()
+elif k == ord("s"):
+    cv.imwrite("logo1.jpg",img)
+    cv.destroyAllWindows()
