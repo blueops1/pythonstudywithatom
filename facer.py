2bdc8be896c2921657bbdd06bd4451e70ba24c4b
blueops1 <hyzfgjj@163.com>
2018-07-07 14:25:19 +0800

    start study face recognition

---
+++ facer.py
@@ -0,0 +1,6 @@
+#! python2.7
+# -*- utf-8 -*-
+#学习人脸识别技术第一弹，加油！
+
+import cv2 as cv
+import numpy as np
--- lesson4.py
+++ lesson4.py
@@ -6,12 +6,22 @@ img2 = cv.imread('logo.png')
 
 # I want to put logo on top-left corner, So I create a ROI
 rows,cols,channels = img2.shape
+print(rows)
+print(cols)
+print(img2.item(0,0,1))
+for x in range(0,rows):
+    for y in range(0,cols):
+        if img2.item(x,y,0) < 240:
+            if img2.item(x,y,1) < 240:
+                if img2.item(x,y,2) <240:
+                    img2.itemset((x,y,0),0)
+                    img2.itemset((x,y,1),0)
 roi = img1[0:rows, 0:cols ]
 # Now create a mask of logo and create its inverse mask also
 img2gray = cv.cvtColor(img2,cv.COLOR_BGR2GRAY)
 cv.imshow('img2gray',img2gray)
 cv.waitKey(0)
-ret, mask = cv.threshold(img2gray, 10, 255, cv.THRESH_BINARY)
+ret, mask = cv.threshold(img2gray, 38, 245, cv.THRESH_BINARY)
 cv.imshow('mask',mask)
 cv.waitKey(0)
 mask_inv = cv.bitwise_not(mask)
