4c245cdc300633c8bcc260ac261b2521a399be0c
blueops1 <hyzfgjj@163.com>
2018-07-07 14:56:57 +0800

    facer 1

--- facer.py
+++ facer.py
@@ -1,6 +1,36 @@
-#! python2.7
-# -*- utf-8 -*-
-#学习人脸识别技术第一弹，加油！
-
 import cv2 as cv
 import numpy as np
+
+cap = cv.VideoCapture(0)
+
+while(1):
+
+    #Take each frame
+    _,frame = cap.read()
+
+    #Convert BRG to HSV
+    hsv = cv.cvtColor(frame,cv.COLOR_BGR2HSV)
+
+    #define range of blue color in hsv
+    lower_blue = np.array([110,50,50])
+    upper_blue = np.array([130,255,255])
+
+    # threshold the HSV image to get only blue colors
+    mask = cv.inRange(hsv,lower_blue,upper_blue)
+
+    #bitwise_and mask and original image
+    res = cv.bitwise_and(frame,frame,mask=mask)
+
+    cv.imshow('frame',frame)
+    cv.waitKey(0)
+    cv.imshow('mask',mask)
+    cv.waitKey(0)
+    cv.imshow('res',res)
+    cv.waitKey(0)
+    k = cv.waitKey(5) & 0xFF
+    if k == 27:
+        break
+
+
+
+    cv.destroyAllWindows()
