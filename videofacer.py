3bb76a7cbd0b7c2106ca9d03e4d1d05f494a5e32
blueops1 <hyzfgjj@163.com>
2018-07-08 11:37:21 +0800

    videofacer2

--- videofacer.py
+++ videofacer.py
@@ -4,12 +4,12 @@ import numpy as np
 
 face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-cap = cv.VideoCapture(0)
+cap = cv.VideoCapture(r'source\baby.mp4')
 
 while (1):
     _,frame = cap.read()
     gray = cv.cvtColor(frame,cv.COLOR_BGR2GRAY)
-    faces = face_cascade.detectMultiScale(gray,1.1,3)
+    faces = face_cascade.detectMultiScale(gray,1.2,3)
     print "found{0}faces!".format(len(faces))
     for(x,y,w,h) in faces:
     #方框
