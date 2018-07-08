8c48bd78b7c676009d17bf6fa228cff17b3ed89b
blueops1 <hyzfgjj@163.com>
2018-07-08 08:33:54 +0800

    facedetect 1

--- facedetect.py
+++ facedetect.py
@@ -1,10 +1,12 @@
 import numpy as np
 import cv2 as cv
+
 face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv.CascadeClassifier('haarcascade_eye.xml')
-img = cv.imread('sachin.jpg')
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+img = cv.imread('test.jpg')
+gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+faces = face_cascade.detectMultiScale(gray,1.3,5)
 for (x,y,w,h) in faces:
     cv.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
     roi_gray = gray[y:y+h, x:x+w]
