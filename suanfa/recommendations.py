d2ed7b00c34f734f0f89ba56622a1d973c7293a1
blueops1 <hyzfgjj@163.com>
2018-07-12 16:45:54 +0800

    算法学习1

--- suanfa/recommendations.py
+++ suanfa/recommendations.py
@@ -14,7 +14,8 @@ critics={'Lisa Rose':{'Lady in the Water':2.5,'Snakes on a Plane':3.5,'Just My L
 'Superman Returns':3.0,'The Night Listener':3.0,'You, Me and Dupree':2.0},
 'Jack Matthews':{'Lady in the Water':3.0,'Snakes on a Plane':4.0,'Superman Returns':5.0,
 'The Night Listener':3.0,'You, Me and Dupree':3.5},
-'Toby':{'Snakes on a Plane':4.5,'Superman Returns':4.0,'You, Me and Dupree':1.0}}
+'Toby':{'Snakes on a Plane':4.5,'Superman Returns':4.0,'You, Me and Dupree':1.0},
+'Necho':{'Lady in the Water':3.5,'Snakes on a Plane':1.5,'Superman Returns':5.0}}
 
 #返回一个有关person1 与 person2 的基于距离的相似度评价--欧几里德距离相关度评价
 def sim_distance(prefs,person1,person2):
@@ -77,6 +78,38 @@ def topMatches(prefs,person,n=5,similarity=sim_pearson):
     return scores[0:n]
 
 
+# 利用所有他人评价值的加权平均，为某人提供建议
+def getRecommendations(prefs,person,similarity=sim_pearson):
+    totals = {}
+    simSums = {}
+    for other in prefs:
+        # 不要和自己做比较
+        if other == person:continue
+        sim = similarity(prefs,person,other)
+
+        #忽略评价值为零或小于零的情况
+        if sim<=0:continue
+        for item in prefs[other]:
+
+            #只对自己还未曾看过的影片进行评价
+            if item not in prefs[person] or prefs[person][item]==0:
+                #相似度*评价值
+                totals.setdefault(item,0)
+                totals[item]+=prefs[other][item]*sim
+                #相似度之和
+                simSums.setdefault(item,0)
+                simSums[item]+=sim
+
+    #建立一个归一化的列表
+    rankings=[(total/simSums[item],item) for item,total in totals.items()]
+
+    #返回经过排序的列表
+    rankings.sort()
+    rankings.reverse()
+    return rankings
+
+
+
 
 # 返回所有人中相似度最高的人员名单
 def simest_person(prefs,similarity=sim_distance):
@@ -106,4 +139,5 @@ def simest_person(prefs,similarity=sim_distance):
 if __name__ == '__main__':
     print(simest_person(critics))
     print(simest_person(critics,sim_pearson))
-    print(topMatches(critics,'Toby',n=3))
+    print(topMatches(critics,'Necho',n=3))
+    print(getRecommendations(critics,'Necho'))
